fe23cc349fcf9bf6edb4cf515ffa7a127e85379b
Othmane Mansouri <othmane.mansouri@outlook.com>
2023-01-14 16:10:40 +0100

    Initial commit

---
+++ src/main/java/Helper/Helper.java
@@ -0,0 +1,76 @@
+package Helper;
+
+import Radar.RadarTagmaster;
+import Radar.RadarViking;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.Arrays;
+import java.util.List;
+public class Helper {
+    public static String getSensorTypeFromPostName(String fileName) {
+
+        int postId;
+        // Use regular expression to match the pattern of file name
+        Pattern pattern = Pattern.compile("P(\\d+)(_\\w+)*\\.csv");
+        Matcher matcher = pattern.matcher(fileName);
+
+        // Check if the file name matches the pattern
+        if (matcher.matches()) {
+            // Get the number from the file name
+            postId = Integer.parseInt(matcher.group(1));
+        } else {
+            // Throw an exception if the file name does not match the pattern
+            throw new IllegalArgumentException("Invalid file name: " + fileName);
+        }
+
+        List<Integer> tube_mixtra = Arrays.asList(9, 11, 19, 23, 24, 26);
+        List<Integer> tube_delta = Arrays.asList(15);
+        List<Integer> rada_viking = Arrays.asList(4, 5, 17);
+        List<Integer> rada_tagmaster = Arrays.asList(2, 3);
+        List<Integer> camera_1 = Arrays.asList(1, 6, 7, 8, 10, 12, 13, 14, 16, 18, 21, 22, 25, 27);
+        List<Integer> camera_2 = Arrays.asList(20);
+
+        if (tube_mixtra.contains(postId)) {
+            return "TUBE_MIXTRA";
+        } else if (tube_delta.contains(postId)) {
+            return "TUBE_DELTA";
+        } else if (rada_viking.contains(postId)) {
+            return "RADAR_VIKING";
+        } else if (rada_tagmaster.contains(postId)) {
+            return "RADAR_TAGMASTER";
+        } else if (camera_1.contains(postId)) {
+            return "CAMERA_T1";
+        } else if (camera_2.contains(postId)) {
+            return "CAMERA_T2";
+        } else {
+            // Throw an exception if the post id does doesn't belong to [1-27]
+            throw new IllegalArgumentException("Invalid post id : " + postId);
+        }
+    }
+
+    public static Class<?> getClassFromType(String type) {
+        if (type.equals("RADAR_VIKING")) {
+            return RadarViking.class;
+        }
+        else if (type.equals("RADAR_TAGMASTER")) {
+            return RadarTagmaster.class;
+        }
+        else {
+            throw new IllegalArgumentException("Invalid type : " + type);
+        }
+    }
+
+    public static Class<? extends Mapper> getMapperFromType(String type) {
+        if (type.equals("RADAR_VIKING")) {
+            return RadarViking.RadarMapper.class;
+        }
+        else if (type.equals("RADAR_TAGMASTER")) {
+            return RadarTagmaster.RadarMapper.class;
+        }
+        else {
+            throw new IllegalArgumentException("Invalid type : " + type);
+        }
+    }
+}
---
+++ src/main/java/Radar/RadarTagmaster.java
@@ -0,0 +1,86 @@
+package Radar;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class RadarTagmaster {
+    private static boolean isValidDate(String day,  String hour, String sec){
+        int jour = Integer.parseInt(day);
+        if(jour > 31 || jour <= 0) return false;
+        if( hour.length() > 4 ) return false;
+        if( sec.length() > 4 ) return false;
+        int h = Integer.parseInt(hour);
+        int hh = h/100;
+        int m = h%100;
+        if(hh>=24 || hh<0) return false;
+        if(m>=60 || m<0) return false;
+        int s = Integer.parseInt(sec);
+        int ss = s/100;
+        if(ss>=60 || ss<0) return false;
+        return true;
+    }
+
+    private static boolean isValidCategory(String ser, String type){
+        if(ser.length() != 6) return false;
+        int size = Integer.parseInt(ser.substring(2,6));
+        if(!( type.equals("2R") || type.equals("VL") || type.equals("PL"))) return false;
+        if(size <205 && !type.equals("2R")) return false;
+        if(  size >= 205 && size < 1140 && !type.equals("VL")) return false;
+        if(size >= 1140 && !type.equals("PL")) return false;
+        return true;
+    }
+
+    private static boolean isValidDirection(String direction){
+        int sens = Integer.parseInt(direction);
+        return sens == 1 || sens == 2;
+    }
+
+    public static boolean isValid(String str) {
+        String[] tokens = str.split(",");
+        if (tokens.length != 7)
+            return false;
+        try {
+            if(!isValidDirection(tokens[0])) return false;
+            if(!isValidDate(tokens[1], tokens[2], tokens[3])) return false;
+            if(!isValidCategory(tokens[5], tokens[6])) return false;
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static String adapt(String line){
+        String[] tokens = line.split(",");
+        String str = "";
+        str += tokens[6] + ",";
+        str += tokens[0] + ",";
+        str += tokens[1] + ",";
+        str += ",";
+        int h = Integer.parseInt(tokens[2]);
+        int s = Integer.parseInt(tokens[3]);
+        int hh = h/100;
+        int m = h%100;
+        int ss = s/100;
+        int c = s%100;
+        str += hh + ":" + m + ":" + ss + ":" + c + ",";
+        str += tokens[4].substring(2);
+        return str;
+    }
+
+    public static class RadarMapper
+            extends Mapper<Object, Text, Text, Text> {
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            String[] tokens = line.split(",");
+            if(isValid(line)){
+                context.write(new Text(tokens[1]), new Text(adapt(line)));
+            }
+            // context.write(word, one);
+        }
+    }
+}
