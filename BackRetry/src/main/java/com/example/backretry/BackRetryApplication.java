57b0e7613874bf5124516c7d477fb56f158c9839
EL HIMIR ABDERRAHIM <59322515+abdrrahimelh@users.noreply.github.com>
2023-01-25 17:20:37 +0100

    1st

---
+++ BackRetry/src/main/java/com/example/backretry/BackRetryApplication.java
@@ -0,0 +1,13 @@
+package com.example.backretry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackRetryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackRetryApplication.class, args);
+    }
+
+}
---
+++ BackRetry/src/test/java/com/example/backretry/BackRetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backretry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackRetryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
