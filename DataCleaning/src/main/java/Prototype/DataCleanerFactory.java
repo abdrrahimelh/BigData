20696b4cdacb15a299071acf3cfdbb4fd207b37f
mansouriothmane <mansouriothmane109@gmail.com>
2023-01-26 00:52:56 +0100

    Remove P2 from input

--- DataCleaning/src/main/java/Prototype/DataCleanerFactory.java
+++ DataCleaning/src/main/java/Prototype/DataCleanerFactory.java
@@ -7,25 +7,23 @@ import Radar.RadarTagmaster2;
 import Radar.RadarViking;
 import Tube.Tube;
 
-import java.util.Map;
-
 public class DataCleanerFactory {
-
-    private static final Map<String, DataCleaner> cleaners = Map.of(
-            "RADAR_VIKING", new RadarViking(),
-            "RADAR_TAGMASTER1", new RadarTagmaster1(),
-            "RADAR_TAGMASTER2", new RadarTagmaster2(),
-            "CAMERA_T1", new CameraT1(),
-            "CAMERA_T2", new CameraT2(),
-            "TUBE_MIXTRA", new Tube()
-    );
-
     public static DataCleaner getCleaner(String type) {
-        DataCleaner cleaner = cleaners.get(type);
-        if (cleaner != null) {
-            return cleaners.get(type);
-        } else {
-            throw new IllegalArgumentException("Invalid type: " + type);
+        switch (type) {
+            case "RADAR_VIKING":
+                return new RadarViking();
+            case "RADAR_TAGMASTER1":
+                return new RadarTagmaster1();
+            case "RADAR_TAGMASTER2":
+                return new RadarTagmaster2();
+            case "CAMERA_T1":
+                return new CameraT1();
+            case "CAMERA_T2":
+                return new CameraT2();
+            case "TUBE_MIXTRA":
+                return new Tube();
+            default:
+                throw new IllegalArgumentException("Invalid type: " + type);
         }
     }
 }
